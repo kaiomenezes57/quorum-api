5dd4aa0d949173b1adb3aa0df8b19bbc846610e8
Kaio Menezes <73968667+kaiomenezes57@users.noreply.github.com>
2026-06-24 23:05:57 -0300

    chore: configure project references

--- src/Quorum.Api/Program.cs
+++ src/Quorum.Api/Program.cs
@@ -1,9 +1,13 @@
+using Quorum.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ src/Quorum.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Quorum.Infrastructure.Persistence;
+
+namespace Quorum.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services, 
+            IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+                options.UseNpgsql(connectionString);
+            });
+
+            return services;
+        }
+    }
+}
