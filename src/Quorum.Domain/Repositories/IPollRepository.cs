88416a41e9838d22366784e357853a9d273e6c1e
Kaio Menezes <73968667+kaiomenezes57@users.noreply.github.com>
2026-07-22 18:56:37 -0300

    Update IPollRepository.cs

--- src/Quorum.Domain/Repositories/IPollRepository.cs
+++ src/Quorum.Domain/Repositories/IPollRepository.cs
@@ -5,7 +5,6 @@ namespace Quorum.Domain.Repositories;
 public interface IPollRepository
 {
     Task CreateAsync(Poll poll);
-    
     Task<IReadOnlyList<Poll>> GetAllAsync();
     Task<Poll?> GetByIdAsync(Guid id);
 }
