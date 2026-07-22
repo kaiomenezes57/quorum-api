57ac9e7373757b26d0d4c54d1184a35a8d32767e
Kaio Menezes <73968667+kaiomenezes57@users.noreply.github.com>
2026-07-22 01:16:29 -0300

    feat(tests): add option tests

---
+++ tests/Quorum.Domain.Tests/Entities/OptionTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Quorum.Domain.Entities;
+
+namespace Quorum.Domain.Tests.Entities;
+
+public class OptionTests
+{
+    [Fact]
+    public void AddVote_WithDuplicatedUserId_ShouldReturnFalse()
+    {
+        var pollId = Guid.NewGuid();
+        var userId = Guid.NewGuid();
+
+        var option = new Option("Option A", pollId);
+        
+        var firstResult = option.AddVote(userId);
+        var secondResult = option.AddVote(userId);
+        
+        firstResult.Should().BeTrue();
+        secondResult.Should().BeFalse();
+        option.Votes.Count.Should().Be(1);
+    }
+}
